8c23172350f0729d56c119155adbd53aa35659c6
steakncheese <romell.bermundo@outlook.com>
2022-10-11 19:58:05 -0600

    encounter Caterpie implemented

--- test.js
+++ test.js
@@ -1,11 +1,2 @@
-import rl from "readline-sync";
-let x = 0;
-let y = 0;
-const playerDirection = rl.question(
-  `Where would you like to go? north \[N\], south \[S\], east \[E\], or west \[W\].      Player selected: `
-);
-if (playerDirection === "north" || playerDirection === "n") {
-  y++;
-  console.log(`Moving North. Your location is currently at (${x},${y})`);
-//   return y;
-}
+let a = 0
+
--- tutorial.js
+++ tutorial.js
@@ -1,9 +1,17 @@
 import rl from 'readline-sync';
 let x = 0
 let y = 0
-for (let i = 0; i<5;i++) {
-    const playerAction = rl.question()
-    let previousDirection = ""    
+let i=1
+let enemy1 = 1
+while (true) {
+    if (x==1&&y==1&&enemy1==1){
+        console.log(`A wild Caterpie has appeared`);
+        enemy1--;
+    }
+    const playerAction = rl.question(undefined, {
+        hideEchoBack: true,
+        mask: ''
+      });
     if (playerAction === 'north' || playerAction === 'w') {
     y++; console.log(`Moving North. Your location is currently at (${x},${y})\n`);
     } else if (playerAction === 'south' || playerAction === 's'){
@@ -15,14 +23,9 @@ for (let i = 0; i<5;i++) {
         if (x==-2&&y==0){
         x++; console.log(`You can't move there. You are back at (${x},${y})\n`); 
         } else {
-        console.log(`Moving West. Your location is currently at (${x},${y})\n`); 
+        console.log(`Moving West. Your location is currently at (${x},${y})\n`);
     }
 }
-}
-function playerAtBoundary (){
-    if (previousDirection === west){
-    x++; console.log(`You can't go there. Your location is currently at (${x},${y})\n`);
-    }
 }
 
 
